abb1d6a3656daf3f29d37441780eea90648a4f37
dalfamosni <infdalfamosni@gmail.com>
2017-12-30 18:58:01 +0100

    Initial commit

---
+++ app/src/main/java/com/dam/dani/holamundo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.dam.dani.holamundo;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int valor = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        Context context = getApplicationContext();
+        CharSequence text = null;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+
+        switch (id){
+            case R.id.action_settings:
+                toast.setText("Settings pressed");
+                break;
+            case R.id.action_reset:
+                toast.setText("Reset pressed");
+                break;
+        }
+        toast.show();
+        return super.onOptionsItemSelected(item);
+    }
+}
+
